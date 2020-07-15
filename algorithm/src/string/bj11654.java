18da20c77a706fbd37788b19913a43f3b48d373d
choijy1705 <cjy1705@naver.com>
2020-07-15 20:40:27 +0900

    practice

---
+++ algorithm/src/string/bj11654.java
@@ -0,0 +1,13 @@
+package string;
+import java.util.*;
+public class bj11654 {
+	static Scanner sc = new Scanner(System.in);
+	static String s;
+	public static void main(String[] args) {
+		s = sc.next();
+		
+		System.out.println((int)s.charAt(0));
+
+	}
+
+}
