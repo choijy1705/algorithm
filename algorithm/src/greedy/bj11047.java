6c335e697c5d62b1c320a480d9eff6920972b400
choijy1705 <cjy1705@naver.com>
2020-07-29 17:44:57 +0900

    practice

---
+++ algorithm/src/greedy/bj11047.java
@@ -0,0 +1,28 @@
+package greedy;
+import java.util.*;
+public class bj11047 {
+	static Scanner sc = new Scanner(System.in);
+	static int N,K;
+	public static void main(String[] args) {
+		N = sc.nextInt();
+		K = sc.nextInt();
+		int[] money = new int[N];
+		for(int i=0;i<N;i++) {
+			money[i] = sc.nextInt();
+		}
+		int cnt = 0;
+		int result;
+		for(int i=N-1;i>=0;i--) {
+			if(K >= money[i]) {
+				result = K/money[i];
+				cnt+= result;
+				K -= result*money[i];
+				System.out.println(K);
+			}
+		}
+		
+		System.out.println(cnt);
+
+	}
+
+}
