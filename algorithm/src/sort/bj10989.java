410ffed70a8dc0a9afb5a7083af6da92ca1a383e
choijy1705 <cjy1705@naver.com>
2020-08-07 18:21:22 +0900

    practice

--- algorithm/src/sort/bj10989.java
+++ algorithm/src/sort/bj10989.java
@@ -1,10 +1,37 @@
 package sort;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+ 
 public class bj10989 {
+ 
+    public static void main(String[] args) throws Exception {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[10001];
 
-	}
+        for (int i = 0; i < N; i++) {
+            arr[Integer.parseInt(br.readLine())] ++;
+        }
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+ 
+        for (int i = 1; i < 10000; i++) {
+            if (arr[i] > 0) {
+                for (int j = 0; j < arr[i]; j++) {
 
+                    bw.write(Integer.toString(i) + "\n");
+         
+                }
+            }
+        }
+        br.close();
+        bw.close();
+    }
+ 
 }
+ 
+ 
