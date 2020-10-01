269d58096b98d262e94edf8ada2d0cd01632bb55
choijy1705 <cjy1705@naver.com>
2020-10-01 21:51:17 +0900

    graph

--- algorithm/src/graph/bj1991.java
+++ algorithm/src/graph/bj1991.java
@@ -1,10 +1,78 @@
 package graph;
+import java.util.*;
 
 public class bj1991 {
-
+	static Scanner sc = new Scanner(System.in);
+	static int n;
+	static HashMap<String, List<String>> map ;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		n = sc.nextInt();
+		
+		map = new HashMap<>();
+		
+		
+		for(int i=0;i<n;i++) {
+			String name = sc.next();
+			String left = sc.next();
+			String right = sc.next();
+			
+			List<String> list = new ArrayList<>();
+			list.add(left);
+			list.add(right);
+			
+			map.put(name, list);
+			
+		}
+		
+		preorder("A");
+		System.out.println();
+		inorder("A");
+		System.out.println();
+		postorder("A");
 
+	}
+	private static void postorder(String name) {
+		if(name.equals(".")) {
+			return;
+		}
+		
+		String left = map.get(name).get(0);
+		String right = map.get(name).get(1);
+		
+		postorder(left);
+		postorder(right);
+		System.out.print(name);
+		
+		
+	}
+	private static void inorder(String name) {
+	
+		if(name.equals(".")) {
+			return;
+		}
+		String left = map.get(name).get(0);
+		String right = map.get(name).get(1);
+		
+		inorder(left);
+		System.out.print(name);
+		inorder(right);
+		
+		
+	}
+	private static void preorder(String name) {
+		
+		if(name.equals(".")) {
+			return;
+		}
+		System.out.print(name);
+		
+		String left = map.get(name).get(0);
+		String right = map.get(name).get(1);
+		
+		
+		preorder(left);
+		
+		preorder(right);
 	}
 
 }
