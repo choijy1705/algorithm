3d33a220859fba9dfbb30f8b9eb4ffdff49b0247
choijy1705 <cjy1705@naver.com>
2020-10-25 22:00:00 +0900

    bruteforce

--- algorithm/src/bruteforce/bj1107.java
+++ algorithm/src/bruteforce/bj1107.java
@@ -1,9 +1,28 @@
 package bruteforce;
-
+import java.util.*;
 public class bj1107 {
-
+	static Scanner sc = new Scanner(System.in);
+	static int n,m;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		n = sc.nextInt();
+		m = sc.nextInt();
+		
+		List<Integer> list = new ArrayList<>();
+		for(int i=0;i<=9;i++) {
+			list.add(i);
+		}
+		
+		for(int i=0;i<m;i++) {
+			int a = sc.nextInt();
+			list.remove(a);
+		}
+		
+		for(int i=0;i<6;i++) {
+			
+		}
+		
+		char c = '1';
+		System.out.println(Character.isDigit(c));;
 
 	}
 
--- algorithm/src/greedy/bj1339.java
+++ algorithm/src/greedy/bj1339.java
@@ -1,9 +1,11 @@
 package greedy;
-
+import java.util.*;
 public class bj1339 {
-
+	static Scanner sc = new Scanner(System.in);
+	static int n;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		n = sc.nextInt();
+	
 
 	}
 
