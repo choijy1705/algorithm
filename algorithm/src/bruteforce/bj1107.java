1d43ca3db0d8019d6220f80e22eb78bb96ac9595
choijy1705 <cjy1705@naver.com>
2020-10-26 23:35:49 +0900

    graph

--- algorithm/src/bruteforce/bj1107.java
+++ algorithm/src/bruteforce/bj1107.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class bj1107 {
 	static Scanner sc = new Scanner(System.in);
 	static int n,m;
+	static int min = Integer.MAX_VALUE;
 	public static void main(String[] args) {
 		n = sc.nextInt();
 		m = sc.nextInt();
@@ -18,10 +19,21 @@ public class bj1107 {
 		}
 		
 		for(int i=0;i<6;i++) {
-			
+			StringBuilder sb = new StringBuilder();
+			pick(i,0,sb);
 		}
 	
 
+	}
+	private static void pick(int i, int j,StringBuilder sb) {
+		sb.append(i);
+		
+		int a = Integer.parseInt(sb.toString());
+		
+		min = Math.min(min, Math.abs(n-a));
+		
+		for(int i=0;i<6;)
+		
 	}
 
 }
--- algorithm/src/graph/bj2644.java
+++ algorithm/src/graph/bj2644.java
@@ -1,10 +1,46 @@
 package graph;
-
+import java.util.*;
 public class bj2644 {
+ 
+    static int[][] map;
+    static int n, p1, p2, par_ch;
+    static int[] check;
+    static Scanner sc = new Scanner(System.in);
+    static void BFS(int start) {
+        Queue<Integer> q = new LinkedList<Integer>();
+        q.offer(start);
+        while (!q.isEmpty()) {
+            int x = q.poll();
+            for (int i = 1; i < map.length; i++) {
+                if (map[x][i] == 1 && check[i] == 0) {
+                    q.offer(i);
+                    check[i] = check[x] + 1; // check¿¡ ÀúÀå.
+                }
+            }
+        }
+        System.out.println(check[p2] == 0 ? -1 : check[p2]);
+    }
+ 
+    public static void main(String[] args){
+ 
+        
+        n = sc.nextInt();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+        p1 = sc.nextInt();
+        p2 = sc.nextInt();
+        par_ch = sc.nextInt();
+        map = new int[n + 1][n + 1]; 
+ 
+        for (int i = 0; i < par_ch; i++) {
+   
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            map[a][b] = map[b][a] = 1;
+        }
+        check = new int[n + 1];
+ 
+        BFS(p1);
+ 
+    }
+ 
 }
