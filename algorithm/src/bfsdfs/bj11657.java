dbbbf9d2235ad34e4d8e4a63d0fbf587c08af626
choijy1705 <cjy1705@naver.com>
2020-11-01 23:05:36 +0900

    bellman-ford

--- algorithm/src/bfsdfs/bj11657.java
+++ algorithm/src/bfsdfs/bj11657.java
@@ -1,9 +1,24 @@
 package bfsdfs;
-
+import java.util.*;
 public class bj11657 {
-
+	static Scanner sc = new Scanner(System.in);
+	static int n,m;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		n = sc.nextInt();
+		m = sc.nextInt();
+		
+		
+		int[][] link = new int[n+1][n+1];
+		for(int i=0;i<m;i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int c = sc.nextInt();
+			
+			link[a][b] = c;
+		}
+		
+		
 
 	}
 
