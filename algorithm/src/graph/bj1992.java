b72d1bfefd680a73107a5dcd4f61c4256ff84d34
choijy1705 <cjy1705@naver.com>
2021-01-01 22:21:53 +0900

    graph

--- algorithm/src/graph/bj1992.java
+++ algorithm/src/graph/bj1992.java
@@ -1,47 +1,33 @@
 package graph;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class bj1992 {
-	static Scanner sc = new Scanner(System.in);
 	static int n,m;
-	static boolean[] check;
-	static int result = 0;
-	public static void main(String[] args) {
-			n = sc.nextInt();
-			m = sc.nextInt();
-			
-			int[][] map = new int[n+1][n+1];
-			
-			for(int i=0;i<m;i++) {
-				map[sc.nextInt()][sc.nextInt()] = sc.nextInt();
-			}
-			check = new boolean[n+1];
-			
-			
-			for(int i=1;i<=n;i++) {
-				if(!check[i]) {
-					check[i] = true;
-					findMinBridge(map[i]);
-				}
-			}
-			System.out.println(result);
+	static int[] parent;
+	static PriorityQueue<edge> pq = new PriorityQueue<>();
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		m = Integer.parseInt(br.readLine());
 		
+		parent = new int[n+1];
+			
+	
 	}
-	private static void findMinBridge(int[] bridge) {
-		int min = Integer.MAX_VALUE;
-		int index = 0;
-		for(int i=0;i<bridge.length;i++) {
-			if(bridge[i] != 0 && bridge[i] < min) {
-				min = bridge[i];
-				index=i;
-			}
-		}
-		if(min == Integer.MAX_VALUE) {
-			return;
-		}
-		result += min;
-		check[index] = true;
-		
+	
+	
+	
+}
+class edge implements Comparable<edge>{
+	int s;
+	int e;
+	int v;
+	@Override
+	public int compareTo(edge o) {
+		// TODO Auto-generated method stub
+		return o.v >= this.v ? -1 : 1;
 	}
-
 }
